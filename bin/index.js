5a72e3a9000681cf670bfeaf2dc60f9ecc2910ef
elbarqy <elbarqy.dev@gmail.com>
2022-06-30 22:07:33 -0700

    minor tweak

--- bin/index.js
+++ bin/index.js
@@ -3,8 +3,6 @@ const argv = require('yargs').argv;
 const { initProject } = require('../lib/core/initProject');
 const { entityGenerator } = require('../lib/core/entity.generator');
 const { CREATION_OPTIONS } = require('../lib/global/enums');
-console.log('works');
-
 if (argv._.length > 0) {
 	switch (argv._[0]) {
 		case 'init':
@@ -14,6 +12,7 @@ if (argv._.length > 0) {
 			if (argv._.length < 2) throw new Error('Missing command name');
 			if (Array.from(CREATION_OPTIONS).includes(argv._[0])) {
 				entityGenerator(argv._[1], argv._[0], true);
+				console.log('done');
 			} else {
 				throw new Error(
 					'No such command is found, type help for more info'
